50b10b420b1f88b81607beda119afe312490c828
GopiNath <gopinathmarri7@gmail.com>
2021-10-19 23:16:00 +0530

    initial-commit

---
+++ src/main/java/com/example/demo/Demoapp1Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoapp1Application {
+
+	public static void main(String[] args) {
+		 int num1 = 5, num2 = 15, sum;
+		    sum = num1 + num2;
+
+		    System.out.println("Sum of these numbers: "+sum);
+		SpringApplication.run(Demoapp1Application.class, args);
+	}
+}
